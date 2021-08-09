a80628deca7f3d2d90f17f365fcbb21fce7ec46c
spaceXML <84154282+spaceXML@users.noreply.github.com>
2021-08-09 17:09:04 +0900

    0809
    123

---
+++ boilerplate-mern-stack-master/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,16 @@
+import React, {antd} from 'react'
+
+import { Typography, Button, Form, Message, Input, Icon} from 'antd';
+
+const {Title} = Typography;
+function VideoUploadPage() {
+    return (
+        <div style={{ maxWidth:'700px', margin:'2rem auto'}}>
+            <div style={{ textAlign:'center', marginBottom:'2rem'}}>
+            <Title level={2}>UploadPage Video</Title>
+            </div>
+        </div>
+    )
+}
+
+export default VideoUploadPage
